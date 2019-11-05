c28a8c82b8af6144e8b8d57847e50e1d792c0b8a
fadihanna123 <54924938+fadihanna123@users.noreply.github.com>
2019-11-05 01:48:47 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,4 @@
 <?php
-	// Rapportera alla fel och visa dem.
-	error_reporting(-1);
-	ini_set("display_errors", 1);
 	// Importera klassen datasource.
 	spl_autoload_register(function($class) {
 	    include 'classes/' . $class . '.class.php';
